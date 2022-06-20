951f114d57bae1c6ca60bb209be48d48b4db928b
Tarik012 <tc78200@gmail.com>
2022-06-20 15:34:04 +0200

    Ajout base de donnees

---
+++ Data/APIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APICustomer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICustomer.Data
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICustomer.Models
+    
+{
+    public class Customer
+    {
+        [Key]
+        [Display(Name = "Numéro client")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Le nom ne peut être vide.")]
+        [Display(Name = "Nom")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Le téléphone ne peut être vide")]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Numéro de téléphone")]
+        [RegularExpression(@"^([0-9]{10})$", ErrorMessage = "Le format du numéro de téléphone est invalide.")]
+        public string PhoneNumber { get; set; } = null!;
+
+    }
+}
